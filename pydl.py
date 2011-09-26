dd81793dd2c3e0d0b36c8482376f1b7a754785bd
Brian Tomlinson <darthlukan@gmail.com>
2011-09-25 22:50:12 -0700

    start on actual program, placeholder mostly

---
+++ pydl.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Filename: pydl.py                                                 #
+# Author: Brian Tomlinson <darthlukan@gmai.com>                     #
+# URL: git@github.com:darthlukan/piddle.git                         #
+# Description: A simple CLI download manager written in Python.     #
+# Warning: If you received this program from any source other than  #
+# the above noted URL, please check the source code! You may have   #
+# downloaded a file with malicious code injected.                   #
+# License: GPLv2, Please see the included LICENSE file.             #
+# Note: This software should be considered experimental!            #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import os
+import sys
+import urllib
+import urllib2
+from progressbar import ProgressBar
+
+#Introduce ourselves
+print("""Hello! I am going to ensure that downloading your file, renaming it,
+and specifying where to save it, are as simple as possible.  Let\'s get to it!""")
+
+#Define our globals
+
+urlToGet = raw_input()
+
+# Read more stuff and put something elegent here...
