ef43da7a4936dd2278496abb5f80962664d7a864
Brian Tomlinson <darthlukan@gmail.com>
2011-10-02 00:03:51 -0700

    We can loop over files, thanks to debaux.manuel@gmail.com for his contributions!

--- pydl.py
+++ pydl.py
@@ -15,57 +15,66 @@
 # os and sys are needed to make sure that files and system level stuff
 # are handled properly.  urllib(2) for communications (we are downloading)
 # fileinput handles looping over links in a file (txt for now, csv later)
-# progressbar adds some bling for the user to look at while we work.
+# progressbar adds some bling for the user to look at while we work.  To get
+# progressbar to work, pip2 install progressbar.
 
 import os
 import sys
 import urllib
 import urllib2
 import fileinput
-from progressbar import ProgressBar
+from progressbar import *
 
 #Introduce ourselves
 print("""Hello! I am going to ensure that downloading your file, renaming it,
 and specifying where to save it, are as simple as possible. Let's get to it!""")
 
 # Warn the user about non-existent feature
-print('Be warned! File Looping has not yet been implemented and will cause an exit.')
+print('Be warned! File Looping has been implemented but is experimental.')
 
 # The function that actually gets stuff
-# IT WORKS!!!
-def getStandardDownload():  # Grab the file
+def getDownload():  # Grab the file
     urllib.urlretrieve(urlToGetFile, fileNameToSave)
 
-def getSpecialDownload():   # Import the file, and loop links
-    fileinput.input(fileInput)
-    if fileInput.status() == 0:
-        urllib.urlretrieve(fileInput, folderNameToSave) #placeholder... for now
-    elif fileInput.status() == '':
-        print('Please re-run the program')
-        exit(0)
-    else:
-        print('There was a problem, throw the holy hand grenade!')
-        exit('The holy hand grenade pwnd you just now.')
-
-#Define initial globals
+#Define initial globals,
 specialDownload = raw_input('Do you need to import a file with links?(y/n): ')
 if specialDownload == 'n':
     urlToGetFile = raw_input('Please enter the download URL: ')
-    # raw_input is smarter than me, it allows for entering absolute paths to
-    # desired filenames.  Go figure, the computer taught me something :)
-    fileNameToSave = raw_input("""Enter the desired download location and filename
-    (/home/username/downloads/filename.ext): """)
-    getStandardDownload() # Main download function, no file loops...yet.
+    fileNameToSave = raw_input('Enter the desired filename: ')
+    getDownload()
 elif specialDownload == 'y':
-    print('This feature has not yet been implemented! Please re-run the program')
-    exit("Feature 'file looping' not yet implemented.")
-    #fileInput =
-    #folderNameToSave =
-    #getSpecialDownload()
+    fileNameUrls = raw_input('Enter the filename (with path) that contains URLs: ')
+    baseDir = raw_input('Enter the directory where to download files: ')
+    # Define how to handle pathing, default to preceding '/'
+    if not baseDir.endswith("/"):
+        baseDir+="/"
+    # Grab the file and iterate over each line, this is not yet smart enough
+    # to discern between an actual url and erroneous text, so don't have anything
+    # other than links in your input file!
+    fi = fileinput.input(fileNameUrls)
+    nl=0 #numbers of line
+    for line in fi:
+        nl+=1 # iterate over the next line
+    # Re-read, this will be cleaned up later
+    fi = fileinput.input(fileNameUrls) # reset the fileinput : can't reuse it
+    cl=0 # currentline
+    # Progressbar() stuff, wheee!
+    widgets = ['Overall Progress: ', Percentage(), ' ',
+                   Bar(marker='#',left='[',right=']'),
+                   ' ', ETA(), ' ', FileTransferSpeed()]
+    pbar = ProgressBar(widgets=widgets, maxval=nl)
+    pbar.start()
+    # Done with the prep work, time to do what the user wants
+    for line in fi:
+        urlToGetFile=line[:-1]
+        fileNameToSave=urlToGetFile[urlToGetFile.rfind('/')+1:]
+        getDownload()
+        cl+=1
+        pbar.update(cl)
+    pbar.finish()
+    exit('File Loop complete, goodbye!') #And I'm spent!
 else:
     print('There was a problem with your response, please re-run the program')
     exit("User input invalid or unreadable.")
-
-
 
 
